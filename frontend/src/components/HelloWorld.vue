b10bbfe5598be6873dd4f40f9b07bd53dc960bce
Sebastian Mazur <sebastian.mazur@skygate.io>
2019-07-18 21:28:58 +0200

    fix(app): off docker and nginx

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -14,7 +14,7 @@
 			</v-flex>
 
 			<v-flex mb-4>
-				<h1 class="display-2 font-weight-bold mb-3">seba app</h1>
+				<h1 class="display-2 font-weight-bold mb-3">kupa app</h1>
 				<p class="subheading font-weight-regular">
 					For help and collaboration with other Vuetify developers,
 					<br>please join our online
--- frontend/src/store/modules/userModule.js
+++ frontend/src/store/modules/userModule.js
@@ -4,54 +4,61 @@ import { LOGIN, REGISTER } from '../actions.type';
 import { saveToken, destroyToken } from '../../common/JwtService';
 
 export const userModule = {
-    state: { user: { email: '', username: '' }, isLoading: false },
-    mutations: {
-        [SET_USER]: (state, { user }) => {
-            state.user = user;
-            state.isLoading = false;
-        },
-        [SET_LOADING]: state => (state.isLoading = true)
-    },
-    actions: {
-        [LOGIN]: async ({ commit }, payload) => {
-            commit(SET_LOADING);
-            try {
-                const {
-                    data: { accessToken }
-                } = await HTTP.post('/auth/login', payload);
-                saveToken(accessToken);
-                console.log(accessToken);
-                const { data } = await HTTP.post('/auth/verify', { accessToken });
-                const userData = await HTTP.get(`/user/getUser/${data.id}`);
-                setTimeout(function() {
-                    commit(SET_USER, { user: userData.data });
-                }, 15000);
-                // commit(SET_USER, { user: userData.data });
-            } catch (err) {
-                destroyToken();
-                console.log(err);
-            }
-        },
-        [REGISTER]: async ({ commit }, payload) => {
-            commit(SET_LOADING);
-            try {
-                const { accessToken } = await HTTP.post('/auth/register', payload);
-                saveToken(accessToken);
-                const { data } = await HTTP.post('/auth/verify', { accessToken });
-                const userData = await HTTP.get(`user/getUser/${data.id}`);
-                commit(SET_USER, userData);
-            } catch (err) {
-                destroyToken();
-                console.log(err);
-            }
-        }
-    },
-    getters: {
-        currentUser: state => {
-            return state.user;
-        },
-        isLoading: state => {
-            return state.isLoading;
-        }
-    }
+	state: { user: { email: '', username: '' }, isLoading: false },
+	mutations: {
+		[SET_USER]: (state, { user }) => {
+			state.user = user;
+			state.isLoading = false;
+		},
+		[SET_LOADING]: state => (state.isLoading = true),
+	},
+	actions: {
+		[LOGIN]: async ({ commit }, payload) => {
+			commit(SET_LOADING);
+			try {
+				const {
+					data: { accessToken },
+				} = await HTTP.post('/auth/login', payload);
+				saveToken(accessToken);
+				console.log(accessToken);
+				const { data } = await HTTP.post('/auth/verify', {
+					accessToken,
+				});
+				const userData = await HTTP.get(`/user/getUser/${data.id}`);
+				setTimeout(function() {
+					commit(SET_USER, { user: userData.data });
+				}, 15000);
+				// commit(SET_USER, { user: userData.data });
+			} catch (err) {
+				destroyToken();
+				console.log(err);
+			}
+		},
+		[REGISTER]: async ({ commit }, payload) => {
+			commit(SET_LOADING);
+			try {
+				const { accessToken } = await HTTP.post(
+					'/auth/register',
+					payload,
+				);
+				saveToken(accessToken);
+				const { data } = await HTTP.post('/auth/verify', {
+					accessToken,
+				});
+				const userData = await HTTP.get(`user/getUser/${data.id}`);
+				commit(SET_USER, userData);
+			} catch (err) {
+				destroyToken();
+				console.log(err);
+			}
+		},
+	},
+	getters: {
+		currentUser: state => {
+			return state.user;
+		},
+		isLoading: state => {
+			return state.isLoading;
+		},
+	},
 };
